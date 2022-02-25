c9b8b50019a4babbafb5235135c4a477c42f378b
Isaac Garcia <isaacmg123@gmail.com>
2022-02-25 01:50:03 -0800

    live lyric request, TODO: expiring auth token

--- spotlyrics.py
+++ spotlyrics.py
@@ -8,12 +8,23 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="8536b313f38f4155a21def
                                                redirect_uri="http://localhost:8888/auth",
                                                scope="user-read-playback-state"))
 
-print(sp.current_playback())
+print(sp.current_playback()['item']['album']['images'][0]['url'])
+print()
 # print(sp.track(track_id='USUYG1291802'))
 # https://api.musixmatch.com/ws/1.1/artist.get?artist_id=118&apikey=ae9aa98c34e0bb6029ae6bcef2e9b4a7
 
-BASE_URL = "https://spclient.wg.spotify.com/color-lyrics/v2/track/1i1fxkWeaMmKEB4T7zqbzK/image/https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d0000b273df7cddc2d32ef9df762d5a30?format=json&vocalRemoval=false&market=from_token"
 
+track_id = sp.current_playback()['item']['id']
+art_image_url = sp.current_playback()['item']['album']['images'][0]['url']
+art_image_directory = art_image_url.split("image/")
+substr = art_image_directory[1]
+
+base_url = "https://spclient.wg.spotify.com/color-lyrics/v2/track/" + \
+    str(track_id) + "/image/https%3A%2F%2Fi.scdn.co%2Fimage%2F" + \
+    str(substr) + "?format=json&vocalRemoval=false&market=from_token"
+print(base_url)
+
+print(substr)
 headers = {
     "Host": "spclient.wg.spotify.com",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0",
@@ -21,7 +32,7 @@ headers = {
     "Accept-Language": "en",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://open.spotify.com/",
-    "authorization": "Bearer BQAuJC81Dl2FdG0oF4aFsBkBSDAg1HF5FiUOBSpO9unopJhEQlEXX2SnLr8nVpmuP9P3nvSl6NK_GplMFSgduv_brO3jakxSMGnpStqNkjpZ-NhDXSZ9N4CDyWeP1F6uHub8oL4s-F5ASO6jP49wyatc4wadRCWmQzdnwAIwWXn6gA0GgsK7MDMp7nGwklSq414Ie6KKgfKQJEksWuDox8D5cj2s5CtlUfDsq0zd13JkbEnlFjAW4K-DqzYvklthCSWUGwcoH8zTTT_2KWKefH5DfpFygNl-DNCTdvBk",
+    "authorization": "Bearer BQBsKS3v-m3pTYRiSFdFAOCabjVgtdZe4zeO9geFAZM5QFetpVOhYNCigC_K294kwDorT6udyy3m3eCA-CbX0W1KjsPkjQF-zfW-CPv3cslI5GhVkRU0Y7UTR0STi4Olm5Zt3ytRGJ9SKEYKonXOxFzlZmYrWEU1ILbxdPt_nWW5_zngzM84gcYyYT2KaitLQHR_UW7-ZZ-41QKfuCuczHZUuDNpnven8SX8Fb2TyxiYgEI9Kpx9qZ8Da82hKbYQlKwamJ6eYBxgOm8ImVIE0jZ0WfvJrO5UdzSUOstI",
     "app-platform": "WebPlayer",
     "spotify-app-version": "1.1.81.4.gf0a51a16",
     "Origin": "https://open.spotify.com",
@@ -33,8 +44,9 @@ headers = {
     "Cache-Control": "no-cache",
     "TE": "trailers"}
 
-response = requests.get(BASE_URL, headers=headers)
+response = requests.get(base_url, headers=headers)
 LYRICS_JSON = response.json()
+# print(LYRICS_JSON)
 
 NUM_LINES = len(LYRICS_JSON['lyrics']['lines'])
 print("Number of Lines in the song: " + str(NUM_LINES))
