521c0f67432920bbad3a11c1205896bb87b999b6
Isaac Garcia <isaacmg123@gmail.com>
2022-04-12 02:02:35 -0700

    add change song logic. TODO: query for fresh lyrics on song change

--- spotlyrics.py
+++ spotlyrics.py
@@ -1,22 +1,27 @@
+import time
 import json
 import os
-
+import subprocess
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-
+# clear the terminal when the program starts
 os.system('cls' if os.name == 'nt' else 'clear')
+
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="8536b313f38f4155a21defafccc3de68",
                                                client_secret="2fc41b3c1d6e488b80098033135a9ef5",
                                                redirect_uri="http://localhost:8888/",
                                                scope="user-read-playback-state"))
 
-# exec(open("get_cookie.py").read())
 
-track_name = sp.current_playback()['item']['name']
-track_artist = sp.current_playback()['item']['artists'][0]['name']
-
+try:
+    track_name = sp.current_playback()['item']['name']
+    track_artist = sp.current_playback()['item']['artists'][0]['name']
+except TypeError:
+    print("No song is playing, try again.")
+    exit()
 
 track_id = sp.current_playback()['item']['id']
 art_image_url = sp.current_playback()['item']['album']['images'][0]['url']
@@ -34,7 +39,7 @@ headers = {
     "Accept-Language": "en",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://open.spotify.com/",
-    "authorization": "Bearer BQAe2_dXjE1HOPR_o40GzepdR_5QfZm5_D5KYjyKDGKDPsY5QP32wlRutYzAUg5bGdSSEbmZj2utrQNT609sSQVOwHpC7y706tI0SExzTdwBzwZWCM_mWmDM1p9I9GL_It6TX9SQ41cgC9c1bx3Y-yXN8omqAAZt_mQI3tdf8JgF6AtgFR-i6lz-whdo1e5t4WFeVNlRO3gRTaEII6EJSwLO0MuKW1uoc-hUYg-jE9lq2vTIe_sCr2ckYrWWqU2UHCtER0w-DnA8NOav3FqBlra3DeOZEIpU6ftPveNz",
+    "authorization": "Bearer BQAVbkHmAT5EPzecYg3N_Vm63xo-7icpW4k5spsPQD3NAXutpxyAlqRiYB5vvU7BZWJqE-NMiqycf7zdEzR01cx4g-2PWeuc25LgF5kKXI3wXndqvnrwzioCIvhIKaJQNIB-UM-ItClC4S_zIlQJUuUIBfaZYXi_nr8zMtTFw5Z62OiPFbSOkflhoL-TifNIMDNtu2SYnT4OLfJ_rxGalxug4q0XlUzeIEnLt3V_nS0X5B0LlBleBlxTnUTDJzzggEPqfnn1p9qSzuwjSGIIYVdehpZ5Q71H3Xu5pDm1",
     "app-platform": "WebPlayer",
     "spotify-app-version": "1.1.81.4.gf0a51a16",
     "Origin": "https://open.spotify.com",
@@ -48,8 +53,44 @@ headers = {
 }
 
 response = requests.get(base_url, headers=headers)
-LYRICS_JSON = response.json()
-# print(LYRICS_JSON)
+RESP_CODE = response.status_code
+
+try:
+    LYRICS_JSON = response.json()
+    NUM_LINES = len(LYRICS_JSON['lyrics']['lines'])
+
+except (KeyError, TypeError):
+    if(RESP_CODE == 401):
+        output = subprocess.getoutput('python  get_cookie.py')
+        headers = {
+            "Host": "spclient.wg.spotify.com",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0",
+            "Accept": "application/json",
+            "Accept-Language": "en",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://open.spotify.com/",
+            "authorization": "Bearer " + output,
+            "app-platform": "WebPlayer",
+            "spotify-app-version": "1.1.81.4.gf0a51a16",
+            "Origin": "https://open.spotify.com",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "TE": "trailers"
+        }
+        response = requests.get(base_url, headers=headers)
+
+try:
+    LYRICS_JSON = response.json()
+    code = response.status_code
+
+except:
+    print("no lyrics found for this track")
+    exit()
+
 
 NUM_LINES = len(LYRICS_JSON['lyrics']['lines'])
 
@@ -73,14 +114,22 @@ for i in range(0, len(ms_timestamp)-1):
         current_line = i
         break
 
-# os.system('cls' if os.name == 'nt' else 'clear')
 
-while(True):
+starttime = time.time()
+while True:
+    track_id2 = sp.current_playback()['item']['id']
+    if(track_id != track_id2):
+        print("song has changed")
+
     current_progress = sp.current_playback()['progress_ms']
     if(current_progress >= int(ms_timestamp[current_line])):
-        print(str(current_line+1) + "-" + str(lyrics[current_line]))
+        printed = False
+        print(lyrics[current_line])
         current_line += 1
 
     is_playing = sp.current_playback()['is_playing']
-    if(not(is_playing)):
+    if(not(is_playing) and not printed):
         print("song paused")
+        printed = True
+
+    time.sleep(0.5 - ((time.time() - starttime) % 0.5))
